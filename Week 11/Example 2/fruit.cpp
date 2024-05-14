bcbf9d700cb449e2fecd7b05113c814aeb0f05cb
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-14 15:23:10 +0900

    Update fruit.cpp

--- Week 11/Example 2/fruit.cpp
+++ Week 11/Example 2/fruit.cpp
@@ -8,13 +8,7 @@ fruit::fruit(float p, int n) {
 	price = p;
 	many = n;
 }
-fruit::fruit(float p, int n, int d) {
-	price = p;
-	many = n;
-	day = d;
-}
 void fruit::show_all() {
 	cout << "price : " << price << endl;
 	cout << "How many : " << many << endl;
-	cout << "day : " << day << endl;
 }
