391ab0937ce1f30da71337cc90e99ebc42ccaa64
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-15 20:36:49 +0900

    Create Main.cpp

---
+++ Week 11/연습2)/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "store.h"
+using namespace std;
+int main() {
+	store A, B;
+	B = store(3, 4, 5, 6, 1000, 2000, 2500, 5000);
+	cout << "Size of Store A (add member function 'average()';shows the average price of the store)" << endl;
+	cout << "sizeof(A) = "<<sizeof(A) << endl;
+	return 123;
+}
