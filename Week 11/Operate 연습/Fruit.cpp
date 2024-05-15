2bfdc4f5ec4fc10bfc2f8d5014c68d172a3a7e02
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-15 22:52:55 +0900

    Create Fruit.cpp

---
+++ Week 11/Operate 연습/Fruit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "fruit.h"
+using namespace std;
+float fruit::show() {
+	return price * many;
+}
+fruit::fruit(float p, int n) {
+	price = p;
+	many = n;
+}
+void fruit::show_all() {
+	cout << "price : " << price << endl;
+	cout << "How many : " << many << endl;
+}
