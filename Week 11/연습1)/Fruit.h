4d6895e5eb4fbb9196109df9db9a14aed2e40603
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-15 20:30:57 +0900

    Create Fruit.h

---
+++ Week 11/연습1)/Fruit.h
@@ -0,0 +1,15 @@
+#pragma once
+class fruit {
+public:
+	// constructor
+	fruit() {
+		price = 200; many = 2; // default values
+	}
+	fruit(float p, int n);
+	// member data
+	int many;
+	float price;
+	// member function
+	float show();
+	void show_all();
+};
