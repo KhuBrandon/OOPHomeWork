1aaf3db585b9075c55606eb4ecd9d62bfe7f2749
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-20 19:54:54 +0900

    Create SUV.h

---
+++ Week 12/PPT 2/Problem 1/SUV.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Car.h"
+class SUV : public Car {
+private:
+	int spareWheels;
+	void Color(int); // private 추가
+public:
+	void klaxon(int);
+	void setSparewheels(int);
+	int getSparewheels();
+	float getTotalPrice();
+};
