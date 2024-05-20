8d88ea40e18c20dc58d3ac4015ed71a4f4963a4d
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-20 22:43:02 +0900

    Create Car.h

---
+++ Week 12/PPT 2/Problem 3/Car.h
@@ -0,0 +1,23 @@
+#pragma once
+class Car {
+public:
+	Car() :speed(0.1), wheels(0), price(0.2) {};
+	Car(float, int, float);
+	void setPrice(float);
+	float getPrice();
+	float speed;
+	void speedChange(float);
+	int wheels;
+	void setWheels(int);
+	int getWheels();
+	void klaxon(int);
+	void weight(int); //public 추가
+    void people(); //virtual 추가
+
+	friend class Engineer;
+protected:
+	float price;
+	void speedUp(float);
+	void speedDown(float);
+	void CarSeat(int); // protected 추가
+};
