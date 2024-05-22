d4b81e057ed01a929ff661451860030a0f9bb674
KimBrandon <163233156+KhuBrandon@users.noreply.github.com>
2024-05-22 21:02:18 +0900

    Update Engineer.cpp

--- Week 12/PPT 2/Problem 2/Engineer.cpp
+++ Week 12/PPT 2/Problem 2/Engineer.cpp
@@ -1,4 +1,10 @@
 #include "Engineer.h"
 int Engineer::getCarPrice(Car* xx) {
-	return xx->price; // access protected or private
+	return xx->price; 
+}
+void Engineer::getWeight(Car* xx) {
+	 xx->weight(4500);
+}
+void Engineer::getKlaxon(Car* xx) {
+	xx->klaxon(2);
 }
